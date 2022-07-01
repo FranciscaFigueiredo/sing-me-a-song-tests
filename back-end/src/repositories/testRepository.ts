17408579fb529ec5963615fc80f395bae0d2dac1
iagod <iago.tostes@cubos.io>
2022-07-01 14:57:12 -0300

    Preparando revisão de código

--- back-end/src/repositories/testRepository.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from '../database';
-
-async function removeAllRecommendations() {
-    await prisma.recommendation.deleteMany();
-}
-
-export const testRepository = {
-    removeAllRecommendations,
-};
--- back-end/src/services/testService.ts
+++
@@ -1,9 +0,0 @@
-import { testRepository } from '../repositories/testRepository';
-
-async function deleteAll() {
-    await testRepository.removeAllRecommendations();
-}
-
-export const testService = {
-    deleteAll,
-};
